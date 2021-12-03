3974f0b46269d76ba4e18b69f03623a5f4288415
unknown <gokalpcakar@gmail.com>
2021-12-03 18:12:01 +0300

    Startup'ta interface ile class ilişkilendirildi.

--- Hafta2_Extensions_MVC/Startup.cs
+++ Hafta2_Extensions_MVC/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Model.Abstract;
+using Model.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace Hafta2_Extensions_MVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // interface ile class iliþkilendiriliyor.
+            services.AddScoped<IMovieRepo, MovieRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
