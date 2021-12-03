bb7155d29f348586e070f9e00913c7d5e4635f6d
unknown <gokalpcakar@gmail.com>
2021-12-03 18:09:06 +0300

    interface'ten alınan metot konsola uygulandı.

--- Hafta2_Console_App/Program.cs
+++ Hafta2_Console_App/Program.cs
@@ -17,6 +17,18 @@ namespace PratikConsoleApp
             Console.WriteLine("Los Angeles Saati: " + Extensions.toLosAngelesTimeZone(trSaati));
             Console.WriteLine("Tokyo Saati: " + Extensions.toTokyoTimeZone(trSaati));
 
+            // interface'ten alınan metodu movirepo'da kullandık.
+            MovieRepo movie = new MovieRepo();
+
+            // MovieRepo'da kullandığımız metodu burada bastırdık.
+            movie.welcomeToMovieRepo();
+
+            // film'in alanlarına ve tiplerine reflection ile ulaşıldı.
+            movie.GetType().GetProperties().ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Name} adlı alanın tipi: {x.GetType().Name}");
+            });
+
             Console.ReadLine();
         }
     }
