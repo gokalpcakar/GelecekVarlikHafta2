78b7d9a0a95145861ac810b806a84327627b5467
unknown <gokalpcakar@gmail.com>
2021-12-03 18:03:29 +0300

    movie interface ve class'ı eklendi

---
+++ Model/Abstract/IMovieRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Abstract
+{
+    // Burada konsol uygulamasında kullanacağımız property'ler tanımlandı.
+    public interface IMovieRepo
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        void welcomeToMovieRepo();
+    }
+}
---
+++ Model/Concrete/MovieRepo.cs
@@ -0,0 +1,23 @@
+﻿using Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Concrete
+{
+    // IMovieRepo interface'inde bulunan tanımlamalar uygulandı.
+    public class MovieRepo : IMovieRepo
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public void welcomeToMovieRepo()
+        {
+            Console.WriteLine("Welcome To Movie Repo!!!");
+        }
+    }
+}
