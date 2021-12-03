f2ffb17db5fbd0df8a862e219239b5d2937047ab
unknown <gokalpcakar@gmail.com>
2021-12-03 18:05:13 +0300

    Extension oluşturuldu.

---
+++ Model/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Extensions
+{
+    public static class Extensions
+    {
+        // Los Angeles saat dilimine dönüştüren fonksiyon.
+        public static string toLosAngelesTimeZone(this DateTime trSaati)
+        {
+            return trSaati.AddHours(1).ToLongTimeString();
+        }
+
+        // Tokyo saat dilimine dönüştüren fonksiyon.
+        public static string toTokyoTimeZone(this DateTime trSaati)
+        {
+            return trSaati.AddHours(6).ToLongTimeString();
+        }
+    }
+}
