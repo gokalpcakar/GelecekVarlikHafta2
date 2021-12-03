1b5b0027b4d415004ac1f641ca8573d4ab267bd4
unknown <gokalpcakar@gmail.com>
2021-12-03 18:07:58 +0300

    Konsolda extension'lar kullanıldı.

--- Hafta2_Console_App/Program.cs
+++ Hafta2_Console_App/Program.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Model.Concrete;
+using Model.Extensions;
+using System;
+using System.Linq;
 
-namespace Hafta2_Console_App
+namespace PratikConsoleApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // Şuanki saat alındı.
+            // tokyo ve la saatlerine dönüştüren extension'lar kullanıldı.
+            DateTime trSaati = DateTime.Now;
+
+            Console.WriteLine("Los Angeles Saati: " + Extensions.toLosAngelesTimeZone(trSaati));
+            Console.WriteLine("Tokyo Saati: " + Extensions.toTokyoTimeZone(trSaati));
+
+            Console.ReadLine();
         }
     }
 }
